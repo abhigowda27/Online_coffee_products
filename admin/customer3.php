ca73f20d4a4d8b2b6a0746cd077952abed1b3ae4
abhigowda27 <136908697+abhigowda27@users.noreply.github.com>
2023-11-12 19:50:30 +0530

    Add files via upload

---
+++ admin/customer3.php
@@ -0,0 +1,98 @@
+<?php
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "details";
+     
+   // connect the database with the server
+   $conn = new mysqli($servername,$username,$password,$dbname);
+     
+    // if error occurs 
+    if ($conn -> connect_errno)
+    {
+       echo "Failed to connect to MySQL: " . $conn -> connect_error;
+       exit();
+    }
+  
+    $sql = "select * from info";
+    $result = ($conn->query($sql));
+    //declare array to store the data of database
+    $row = []; 
+  
+    if ($result->num_rows > 0) 
+    {
+        // fetch all data from db into array 
+        $row = $result->fetch_all(MYSQLI_ASSOC);  
+    }   
+?>
+  
+<!DOCTYPE html>
+<html>
+<head>
+<title>Customer details</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/morris.css" type="text/css"/>
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<script src="js/jquery-2.1.4.min.js"></script>
+<!-- //jQuery -->
+<!-- tables -->
+<link rel="stylesheet" type="text/css" href="css/table-style.css" />
+<link rel="stylesheet" type="text/css" href="css/basictable.css" />
+<!-- //tables -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+</head>
+<style>
+    td,th {
+        border: 1px solid black;
+        padding: 10px;
+        margin: 5px;
+        text-align: center;
+    }
+</style>
+  
+<body>
+<h2>Customer Details</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>phone</th>
+                <th>Email</th>
+                <th>address</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+               if(!empty($row))
+               foreach($row as $rows)
+              { 
+            ?>
+            <tr>
+  
+                <td><?php echo $rows['name']; ?></td>
+                <td><?php echo $rows['phone']; ?></td>
+                <td><?php echo $rows['email']; ?></td>
+                <td><?php echo $rows['address']; ?></td>
+  
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
+  
+<?php   
+    mysqli_close($conn);
+?>
---
+++ php/header.php
@@ -0,0 +1,78 @@
+
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="index.html" class="navbar-brand">
+            <h3 class="px-5">
+                <i class="fas fa-home"></i>HOME 
+            </h3>
+        </a>
+        <button class="navbar-toggler"
+            type="button"
+                data-toggle="collapse"
+                data-target = "#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+        ><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+l
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#menu">menu</a>
+        <a href="#products">products</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+        <a href="#blogs">blogs</a>
+    </nav>
+
+
+    <div class="icons">
+        <div class="fas fa-search" id="search-btn"></div>
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-bars" id="menu-btn"></div>
+    </div>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="mr-auto"></div>
+            <div class="navbar-nav">
+                <a href="cart.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                        <i class="fas fa-shopping-cart"></i> Cart
+                        <?php
+
+                        if (isset($_SESSION['cart'])){
+                            $count = count($_SESSION['cart']);
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                        }else{
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                        }
+
+                        ?>
+                    </h5>
+                </a>
+            </div>
+        </div>
+
+    </nav>
+</header>
+
+
+
+
+
+
